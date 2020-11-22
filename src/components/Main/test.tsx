0e56d0c27c1ea8b98653796630053c8231a9e387
Rafael Souza <devrafaelsouza@gmail.com>
2020-11-22 18:39:08 -0300

    Criando estilos no primeiro componente e Melhorando snapshots com jest-styled-components.

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,10 +4,18 @@ import Main from './index';
 
 describe('<Main />', () => {
   it('Deve renderizar o header', () => {
-    render(<Main />);
+    const { container } = render(<Main />);
 
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('Deve renderizar as cores corretamente', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
